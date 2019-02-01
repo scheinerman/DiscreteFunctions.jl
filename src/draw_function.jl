547f5374476b07b6fd388735288ad6b0a9752af6
Ed Scheinerman <ers@jhu.edu>
2019-02-01 15:24:57 -0500

    new stuff

--- src/draw_function.jl
+++ src/draw_function.jl
@@ -16,8 +16,9 @@ function func2graph(f::DiscreteFunction)
     return G
 end
 
-
-
+"""
+`draw(f::DiscreteFunction)` draws a directed graph representation of `f`.
+"""
 function draw(f::DiscreteFunction)
     n = length(f)
     newdraw()
@@ -26,12 +27,6 @@ function draw(f::DiscreteFunction)
     xy = G.cache[:GraphEmbedding].xy
 
     # xy = [ (n*sin(2*pi*t/n),n*cos(-2*pi*t/n)) for t=0:n-1 ]
-
-    for j=1:n
-        x,y = xy[j]
-        draw_point(x,y,color=:black,marker=1)
-        annotate!(1.1x,1.1y,text(string(j),6))
-    end
 
     mu = 0.9
     for j=1:n
@@ -43,6 +38,12 @@ function draw(f::DiscreteFunction)
         else
             draw_circle(a,b,0.1,color=:black)
         end
+    end
+
+    for j=1:n
+        x,y = xy[j]
+        # draw_point(x,y,color=:black,marker=1)
+        annotate!(x,y,text(string(j),8,color=:red))
     end
 
     finish()
