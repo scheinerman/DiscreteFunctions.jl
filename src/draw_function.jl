6a833c4a09b6985c5d366e3b3734e13664b6893f
Ed Scheinerman <ers@jhu.edu>
2019-02-01 10:48:02 -0500

    Better drawing of discrete functions

--- src/draw_function.jl
+++ src/draw_function.jl
@@ -1,20 +1,39 @@
-using DiscreteFunctions, SimpleDrawing, Plots
+using DiscreteFunctions, SimpleDrawing, Plots, SimpleGraphs
 
 import SimpleDrawing.draw
+
+
+function func2graph(f::DiscreteFunction)
+    n = length(f)
+    G = IntGraph(n)
+    for a=1:n
+        b = f(a)
+        if a!=b
+            add!(G,a,b)
+        end
+    end
+    embed(G,:combined)
+    return G
+end
+
+
 
 function draw(f::DiscreteFunction)
     n = length(f)
     newdraw()
 
-    xy = [ (n*sin(2*pi*t/n),n*cos(-2*pi*t/n)) for t=0:n-1 ]
+    G = func2graph(f)
+    xy = G.cache[:GraphEmbedding].xy
+
+    # xy = [ (n*sin(2*pi*t/n),n*cos(-2*pi*t/n)) for t=0:n-1 ]
 
     for j=1:n
         x,y = xy[j]
         draw_point(x,y,color=:black,marker=1)
-        annotate!(1.1x,1.1y,string(j))
+        annotate!(1.1x,1.1y,text(string(j),6))
     end
 
-    mu = 0.95
+    mu = 0.9
     for j=1:n
         fj = f(j)
         a,b = xy[j]
@@ -22,7 +41,7 @@ function draw(f::DiscreteFunction)
         if fj!=j
             draw_vector(mu*(c-a),mu*(d-b),a,b,color=:black)
         else
-            draw_circle(a,b,0.25,color=:black)
+            draw_circle(a,b,0.1,color=:black)
         end
     end
 
